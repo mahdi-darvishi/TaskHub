5eb2117a5a010765b99ccf1a573c4091495e6f47
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-12-07 13:44:38 +0330

    create sign-up page

--- frontend/app/routes/auth/sign-up.tsx
+++ frontend/app/routes/auth/sign-up.tsx
@@ -1,5 +1,146 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { signUpSchema } from "@/lib/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router";
+import { z } from "zod";
+
+type SignupFormData = z.infer<typeof signUpSchema>;
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const form = useForm<SignupFormData>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      email: "",
+      name: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const handleSubmit = (values: SignupFormData) => {
+    console.log(values);
+  };
+  return (
+    <div className=" min-w-md min-h-screen flex flex-col items-center justify-center bg-muted/40 p-4">
+      <Card className="w-full shadow-xl">
+        <CardHeader className="text-center mb-5">
+          <CardTitle className="text-2xl font-bold">
+            Create an account
+          </CardTitle>
+          <CardDescription className="text-sm text-muted-foreground">
+            create an account to continue
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(handleSubmit)}
+              className="space-y-6"
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="email@example.come"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Full Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        placeholder="Mehdi Darvishi"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="********"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="********"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button type="submit" className="w-full">
+                Sign in
+              </Button>
+            </form>
+          </Form>
+
+          <CardFooter className="flex items-center justify-center mt-6">
+            <div>
+              <p className=" text-sm text-muted-foreground">
+                Already have an account? <Link to="/sign-in"> Sign in</Link>
+              </p>
+            </div>
+          </CardFooter>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default SignUp;
