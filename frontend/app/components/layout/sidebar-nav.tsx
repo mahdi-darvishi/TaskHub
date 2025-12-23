ec32ab607e281253f669f89fb5da12e49f71d7cb
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-12-23 15:07:02 +0330

    feat: add Header and Sidebar components with navigation and workspace selection functionalities

---
+++ frontend/app/components/layout/sidebar-nav.tsx
@@ -0,0 +1,64 @@
+import { cn } from "@/lib/utils";
+import type { Workspace } from "@/types/indedx";
+import type { LucideIcon } from "lucide-react";
+import { Button } from "../ui/button";
+import { useLocation, useNavigate } from "react-router";
+
+interface SidebarNavProps extends React.HTMLAttributes<HTMLElement> {
+  items: {
+    title: string;
+    href: string;
+    icon: LucideIcon;
+  }[];
+  isCollapsed: boolean;
+  currentWorkspace: Workspace | null;
+  className?: string;
+}
+
+const SidebarNav = ({
+  items,
+  isCollapsed,
+  className,
+  currentWorkspace,
+  ...props
+}: SidebarNavProps) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  return (
+    <nav className={cn("flex flex-col gap-y-2", className)} {...props}>
+      {items.map((item) => {
+        const Icon = item.icon;
+        const isActive = location.pathname === item.href;
+        const handleOnClick = () => {
+          if (item.href === "/dashboard") {
+            navigate(item.href);
+          } else if (currentWorkspace?._id) {
+            navigate(`${item.href}?workspaceId=${currentWorkspace._id}`);
+          } else {
+            navigate(item.href);
+          }
+        };
+        return (
+          <Button
+            key={item.href}
+            variant={isActive ? "outline" : "ghost"}
+            className={cn(
+              "justify-start",
+              isActive && "bg-blue-800/20 text-blue-600 font-medium"
+            )}
+            onClick={handleOnClick}
+          >
+            <Icon className="mr-2 size-4" />
+            {isCollapsed ? (
+              <span className="sr-only">{item.title}</span>
+            ) : (
+              item.title
+            )}
+          </Button>
+        );
+      })}
+    </nav>
+  );
+};
+
+export default SidebarNav;
