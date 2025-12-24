7be8c82367830a1e1a861139af5c3362ae369196
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-12-25 00:04:36 +0330

    feat: update routing to include project and task details pages; enhance type imports

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -23,5 +23,14 @@ export default [
       "workspaces/:workspaceId",
       "routes/dashboard/workspaces/workspace-details.tsx"
     ),
+    route(
+      "workspaces/:workspaceId/projects/:projectId",
+      "routes/dashboard/project/project-details.tsx"
+    ),
+
+    route(
+      "workspaces/:workspaceId/projects/:projectId/tasks/:taskId",
+      "routes/dashboard/task/task-details.tsx"
+    ),
   ]),
 ] satisfies RouteConfig;
